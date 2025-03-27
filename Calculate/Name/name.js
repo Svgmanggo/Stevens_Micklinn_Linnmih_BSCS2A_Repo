e1fa1a8ec68b143fa8b87a0bf179adbe25287f10
Svgmanggo <sweetmicklinn5@gmail.com>
2025-03-27 17:13:03 +0800

    Name andCaclculate

---
+++ Calculate/Name/name.js
@@ -0,0 +1,6 @@
+function printName(firstname, lastname) {
+    var fullNamed = firstname + ' ' + lastname
+    return fullNamed
+}
+
+console.log('My Fullname is ' +printName( 'Micklinn Linnmih', 'Stevens'))
